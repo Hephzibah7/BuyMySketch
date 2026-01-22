31b83e1e6998472acbc664f7dc5bc8dbccfe077c
Hephzibah Ranjan <hephzibahranjan@gmail.com>
2026-01-22 15:03:27 +0530

    refactor:Added authentication folder

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Login from './component/Login';
-import SignUp from './component/Signup';
+import Login from './authentication/Login';
+import SignUp from './authentication/Signup';
 function App() {
   return (
     <div className="App">
