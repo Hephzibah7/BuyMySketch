cfb22c54dd0bf569a3116917585fdd5ac6759543
Hephzibah Ranjan <hephzibahranjan@gmail.com>
2026-01-22 14:44:53 +0530

    feat:Added css to the login code

--- frontend/src/component/login.tsx
+++ frontend/src/component/login.tsx
@@ -3,18 +3,38 @@ import React from 'react';
 const Login = () => {
   return (
     <div className='h-screen flex items-center justify-center'>
-        <div className='w-1/3 h-5/6 bg-gray-200'>
-            <div className='p-5'>
-                <div className='m-10'>
-                   <div className='p-3 font-bold text-black text-4xl'>
-                    LOGIN
-                   </div>
-                   <div className=''>
-                        <div>
-                            
+        <div className='w-full m-2 h-2/3 md:w-2/3 lg:w-2/5 border border-pink-400 border-2 rounded-2xl'>
+            <div className='lg:p-5'>
+                <form>
+                <div className='m-10 lg:m-10'>
+                   <div className='text-center font-bold'>
+                        <div className='text-3xl text-blue-500'>
+                            BuyMySketch
+                        </div>
+                        <div className='mt-8 mb-10 text-xl'>
+                            Enter your login credentials
                         </div>
                    </div>
+                   <div className=''>
+                        <div className='flex flex-col gap-8'>
+                            <div>
+                                <h1 className='font-bold text-xl mb-2'>Username:</h1>
+                                <input className="border  border-black w-full p-2" placeholder='Enter your Username'/>
+                            </div>
+                            <div>
+                                <h1 className='font-bold text-xl mb-2'>Password:</h1>
+                                <input className="border border-black w-full p-2" placeholder='Enter your Password'/>
+                            </div>
+                        </div>
+                   </div>
+                   <div className='mt-8'>
+                    <button className='w-full border p-3 bg-blue-500 rounded-xl text-white'>Submit</button>
+                   </div>
+                   <div className='mt-8 flex justify-center font-bold'>
+                        Not registered?  <span className='text-pink-600 cursor'>&nbsp; Create an account</span>
+                   </div>
                 </div>
+                </form>
             </div>
         </div>
     </div>
