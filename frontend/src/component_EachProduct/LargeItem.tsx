c5b6084bf08d949bcc94a25ab5c159f5f5c20f76
Hephzibah Ranjan <hephzibahranjan@gmail.com>
2026-01-26 21:11:02 +0530

    refactor:refactored item component

---
+++ frontend/src/component_EachProduct/LargeItem.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function LargeItem() {
+  return (
+    <div>
+      <div className='w-full h-contain bg-green-400'>
+      hello
+       
+      </div>
+    </div>
+  )
+}
+
+export default LargeItem
--- frontend/src/component_EachProduct/Product.tsx
+++ frontend/src/component_EachProduct/Product.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import Item from './Item';
+import LargeItem from './LargeItem';
 
 function Product() {
   return (
     <div>
-    <div className='md:hidden'>
+    <div className='flex md:hidden'>
       <Item/>
+    </div>
+    <div className='hidden md:flex'>
+      <LargeItem/>
     </div>
     </div>
   )
